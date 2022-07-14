2fc728cf5e85f111a2c0cd27d80a1d1262605652
Tan Wang <107713305+wangruohai@users.noreply.github.com>
2022-07-14 10:01:26 +0800

    Update test_grad_tf_CPU_sparse.py

--- experiment/test_grad_tf/test_grad_tf_CPU_sparse.py
+++ experiment/test_grad_tf/test_grad_tf_CPU_sparse.py
@@ -1,5 +1,6 @@
 """
-This module contains the experiments for the paper
+This module contains the experiments for the RNN inspired simulation approach for large-scale inventory optimization problems
+discussed in the paper, "Large-Scale Inventory Optimization: A Recurrent-Neural-Networks-Inspired Simulation Approach".
 
 Author:
     Tan Wang
@@ -12,30 +13,16 @@ from time import time
 
 
 
-def evaluate_performance_of_gradient_computation_tf_CPU(I_S_0, sim, rep_num, print_flag=True):
-    # sim.simulate_and_bp_tf(I_S=I_S_0, GPU_flag=False, rand_seed=0)#warm up
-    t_s = time()
-    for i in range(rep_num):
-        sim.simulate_and_bp_tf(I_S=I_S_0, GPU_flag=False, rand_seed=i)
-    if print_flag:
-        print_run_time('Gradient Computation Using TensorFlow-CPU-sparse', t_s, 'second', rep_num)
-
-
-
-
 
 
 if __name__ == "__main__":
     data_path = "./data"
     data_type = np.float32
-    nodes_num_list = [1000, 5000, 10000, 50000, 100000, 500000]#[1000, 5000, 10000, 50000, 100000, 500000]#[1000]#
-    warm_flag = True
-    for n in nodes_num_list:
-        sim = simulation.Simulation(data_type=data_type, duration=100, data_path=data_path,
-                                    network_name='test_bom_' + str(n) + '.pkl',
-                                    penalty_factor=2.0)
-        I_S_0 = data_type(10) * np.ones((1, n), dtype=data_type)
-        if warm_flag:
-            evaluate_performance_of_gradient_computation_tf_CPU(I_S_0, sim, 1, False)#warm up
-            warm_flag =False
-        evaluate_performance_of_gradient_computation_tf_CPU(I_S_0, sim, 10)
+    nodes_num = 1000 # set number of nodes here
+    sim = simulation.Simulation(data_type=data_type, duration=100, data_path=data_path,
+                                network_name='test_bom_' + str(nodes_num) + '.pkl',
+                                penalty_factor=2.0)
+    I_S_0 = data_type(10) * np.ones((1, nodes_num), dtype=data_type)
+    t_s = time()
+    sim.simulate_and_bp_tf(I_S=I_S_0, GPU_flag=False, rand_seed=0) #you can use other random seeds
+    print_run_time('Gradient Computation Using TensorFlow-CPU-sparse', t_s, 'second', 1)
